bb5756b88df85006b36bbc265b7e99056725e8d3
Wenzel Barboza <wenzelbarboza@Wenzels-MacBook-Air.local>
2023-03-20 17:52:45 +0200

    added styling to the display card

--- src/components/DrinkItem.jsx
+++ src/components/DrinkItem.jsx
@@ -16,9 +16,9 @@ const DrinkItem = ({ drinkName, glass, type, image, id }) => {
     return (
         <div className='cardShadow bg-gray-100 flex flex-col h-[24rem] w-60 rounded m-9 justify-around' >
             <img src={image} alt="drink image" className=' rounded-t' />
-            <h3 className='ml-2'>{drinkName}</h3>
-            <h5 className='ml-2'>{glass}</h5>
-            <h6 className='ml-2'>{type}</h6>
+            <h3 className='ml-2 text-3xl'>{drinkName}</h3>
+            <h5 className='ml-2 '>{glass}</h5>
+            <h6 className='ml-2 text-sm text-gray-600'>{type}</h6>
             <div>
                 <button onClick={() => handleNavigate(id)} className=' bg-blue-600 ml-2 px-4 py-1 rounded border-none text-white '>details</button>
             </div>
